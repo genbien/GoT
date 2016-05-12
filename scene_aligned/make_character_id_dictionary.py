e17cba344eea09e10fffa30ff11b0ed25416072f
genbien <genbien@gmail.com>
2016-05-12 16:59:15 +0200

    mod: compatibility with online wiki and tvd library

--- scene_aligned/make_character_id_dictionary.py
+++ scene_aligned/make_character_id_dictionary.py
@@ -1,8 +1,22 @@
-print "characters = {"
+from __future__ import print_function
+
+out_file = open('character_dictionary.py','w')
+
+print("characters = {", file=out_file)
 i = 0
 with open('season1_got_list_characters.txt') as f:
 	for line in f:
 		line = line.rstrip()
-		print "\t'"+line+"': "+str(i)+","
+
+		## compatability between online wiki list and TVD list
+		if line == "Drogo":
+			line = "Khal Drogo"
+		if line == "Luwin":
+			line = "Maester Luwin"
+		if line == "Mordane":
+			line = "Septa Mordane"
+
+		## looks like: "DAENERYS_TARGARYEN": 133,
+		print("\t\""+line.replace(" ", "_").upper()+"\": "+str(i)+",", file=out_file)
 		i += 1
-print	"}"
+print("}", file=out_file)
