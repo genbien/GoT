c60e3a02ad67c9a59a1604e43f3b6f9f745587c1
genbien <genbien@gmail.com>
2016-05-26 16:14:16 +0200

    mod: alignment evaluation tool progress

--- alignment_evaluation/evaluate_alignment.py
+++ alignment_evaluation/evaluate_alignment.py
@@ -1,16 +1,52 @@
+################################################################################
+##                                                                            ##
+## program to evaluate the accuracy of automatically aligned                  ##
+## subtitles and transcripts for Game of Thrones.                             ##
+##                                                                            ##
+################################################################################
+
 import re
 import collections
 import pprint
 
 transcripts_file  = 'GameOfThrones.Season01.Episode01.txt'
 subtitle_file     = 'GameOfThrones.Season01.Episode01.en.srt'
-manual_align_file = 'mod_manual_alignment_subtitles_transcripts_ep01.txt'
+manual_align_file = 'manual_alignment_subtitles_transcripts_ep01.txt'
+## automatically aligned file we want to compare to the manually aligned file
+auto_align_file   = 'aligned_GameOfThrones.Season01.Episode01.txt'
+
+
+def auto_align_to_list(auto_align_file):
+	auto_aligned_text = []
+	with open(auto_align_file) as f:
+		for line in f:
+			line = line.rstrip()
+			## put speaker and line information in the list, ignore time
+			text = line.split(" ", 2)[2]
+			auto_aligned_text.append(text)
+	return auto_aligned_text
+
+
+def manual_align_to_dict(manual_align_file):
+	manual_align = collections.OrderedDict()
+	with open(manual_align_file) as f:
+		align = f.readlines()
+	for line in align:
+		line = line.rstrip()
+		if line.startswith('%'):
+			pass
+		else:
+			line_s = line.split('\t')
+			manual_align[line_s[0]] = line_s[1]
+	return manual_align
+
 
 def transcripts_to_list(transcripts_file):
 	names = []
 	with open(transcripts_file) as f:
 		for line in f:
 			line = line.rstrip()
+			## only keep speaker information
 			name = line.split(" ", 1)[0]
 			names.append(name)
 	return names
@@ -20,6 +56,7 @@ def subtitles_to_list(subtitle_file):
 	subtitles = []
 	with open(subtitle_file) as f:
 		lines = f.read().replace('\n', '')
+	## split on timestamp
 	lines = re.split(r'\d*:\d*:\d*,\d* --> \d*:\d*:\d*,\d*', lines)
 	for line in lines:
 		line_split = re.split(r'- ', line)
@@ -29,18 +66,21 @@ def subtitles_to_list(subtitle_file):
 	return subtitles
 
 
-names = transcripts_to_list(transcripts_file)
-subtitles = subtitles_to_list(subtitle_file)
+auto_aligned_text = auto_align_to_list(auto_align_file)
+manual_align      = manual_align_to_dict(manual_align_file)
+names             = transcripts_to_list(transcripts_file)
+subtitles         = subtitles_to_list(subtitle_file)
 
-# print "length", len(subtitles)
-print names[0], subtitles[0]
+# print "length auto aligned", len(auto_aligned_text)
+# pprint.pprint(auto_aligned_text)
+# print "length names", len(names)
+# pprint.pprint(names)
+# print "length subtitles", len(subtitles)
+# print names[0], subtitles[0]
 
-## indexes all start at 0 (with modify_subtitle_numbers.py)
-with open(manual_align_file) as f:
-	for line in f:
-		line = line.rstrip()
-		if line.startswith('%'):
-			pass
-		else:
-			# print line
-			index = line.split('\t')
+
+# i = 0
+# j = 0
+# while i <= len(subtitles) and j <= len(names):
+# 	j += 1
+# 	i += 1
