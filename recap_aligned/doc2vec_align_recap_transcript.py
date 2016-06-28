565648a7acebe272ec0702bc452353a167da7518
genbien <genbien@gmail.com>
2016-06-28 15:33:55 +0200

    add: new training sources ( + cleaned)

--- recap_aligned/doc2vec_align_recap_transcript.py
+++ recap_aligned/doc2vec_align_recap_transcript.py
@@ -140,10 +140,21 @@ sources = {
     '../../../0_corpus/recaps/recaps_txt/cleaned/GameOfThrones.Season01.Episode05.txt':'SCENES05',
     '../../../0_corpus/manual_transcripts/cleaned/GameOfThrones.Season01.Episode05.txt':'TRANSCRIPTS05',
 
-    '../../../0_corpus/books/AClashOfKings.txt':'CLASHOFKINGS',
-    '../../../0_corpus/books/ADanceWithDragons.txt':'DANCEWITHDRAGONS',
-    '../../../0_corpus/books/AGameOfThrones.txt':'GAMEOFTHRONES',
-    '../../../0_corpus/books/AStormOfSwords.txt':'STROMOFSWORDS'
+    '../../../0_corpus/books/cleaned/AClashOfKings.txt':'CLASHOFKINGS',
+    '../../../0_corpus/books/cleaned/ADanceWithDragons.txt':'DANCEWITHDRAGONS',
+    '../../../0_corpus/books/cleaned/AGameOfThrones.txt':'GAMEOFTHRONES',
+    '../../../0_corpus/books/cleaned/AStormOfSwords.txt':'STROMOFSWORDS',
+
+    '../../../0_corpus/summaries/long_summary/cleaned/GameOfThrones.S01E01.en.txt': 'SUM01',
+    '../../../0_corpus/summaries/long_summary/cleaned/GameOfThrones.S01E02.en.txt': 'SUM02',
+    '../../../0_corpus/summaries/long_summary/cleaned/GameOfThrones.S01E03.en.txt': 'SUM03',
+    '../../../0_corpus/summaries/long_summary/cleaned/GameOfThrones.S01E04.en.txt': 'SUM04',
+    '../../../0_corpus/summaries/long_summary/cleaned/GameOfThrones.S01E05.en.txt': 'SUM05',
+    '../../../0_corpus/summaries/long_summary/cleaned/GameOfThrones.S01E06.en.txt': 'SUM06',
+    '../../../0_corpus/summaries/long_summary/cleaned/GameOfThrones.S01E07.en.txt': 'SUM07',
+    '../../../0_corpus/summaries/long_summary/cleaned/GameOfThrones.S01E08.en.txt': 'SUM08',
+    '../../../0_corpus/summaries/long_summary/cleaned/GameOfThrones.S01E09.en.txt': 'SUM09',
+    '../../../0_corpus/summaries/long_summary/cleaned/GameOfThrones.S01E10.en.txt': 'SUM10',
 }
 
 sentences = LabeledLineSentence(sources)
@@ -153,8 +164,8 @@ model.build_vocab(sentences.to_array())
 for epoch in range(10):
     model.train(sentences.sentences_perm())
 
-model.save('./got.d2v')
-model = Doc2Vec.load('./got.d2v')
+# model.save('./got.d2v')
+# model = Doc2Vec.load('./got.d2v')
 
 
 # scenes are vertical and transcripts are horizontal
